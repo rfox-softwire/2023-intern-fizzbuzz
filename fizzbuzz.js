60373e3d3ebbdfe0dd00e8596dacab6dcbee30a2
Ryan Fox <ryan.fox@softwire.com>
2025-09-09 16:38:47 +0100

    Completed Part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,20 @@
 // This is our main function
 function fizzbuzz() {
-    console.log("Hello, World!");
 
     // Put your code here...
+    for (let i=1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0) {
+            console.log("Fizz")
+        } else if (i % 5 === 0) {
+            console.log("Buzz")
+        } else {
+            console.log(i)
+        }
+    }
+
+
 
 }
 
